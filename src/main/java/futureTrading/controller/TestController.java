43ab2e2cebc753552a9784b032b506b72f590b11
Icyfrog <sjtu-yqc@sjtu.edu.cn>
2020-06-05 19:23:49 +0800

    websocket try 1.0

--- src/main/java/futureTrading/controller/TestController.java
+++ src/main/java/futureTrading/controller/TestController.java
@@ -13,19 +13,76 @@ import futureTrading.daos.TestDao;
 import futureTrading.entities.FuturesOrder;
 import futureTrading.entities.FuturesProduct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import static java.lang.Thread.sleep;
+
+
+@Slf4j
+@Component
+@ServerEndpoint("/connection/{username}")
 @Controller
 @RequestMapping(path = "/test")
 public class TestController {
 
+    // 保存 username --> session 的映射关系
+    // TODO: 后面也许可以写进redis里
+    private ConcurrentHashMap<String, Session> userSessionMap = new ConcurrentHashMap<>();
+
     @Autowired
     TestDao testDao;
+
+    //  TODO: LOG
+    @ResponseBody
+    @OnOpen
+    public String onOpen(Session session, @PathParam("username") String username) {
+        if(userSessionMap.contains(username)) {return "connection failed";}
+        userSessionMap.put(username,session);
+        //log.info("Connection connected");
+        //log.info("username: "+ username + "\t session: " + session.toString());
+        return "connect";
+    }
+
+    //  TODO: LOG
+    @OnClose
+    public void onClose(Session session, @PathParam("username") String username) {
+        userSessionMap.remove(username);
+        //log.info("Connection closed");
+    }
+
+    // 传输消息错误调用的方法
+    @OnError
+    public void OnError(Throwable error) {
+        log.info("Connection error");
+    }
+
+    //  send message to every username
+    @GetMapping(path = "/sendTextTest")
+    public void sendTextTest() throws IOException {
+        for(Map.Entry<String, Session> entry:userSessionMap.entrySet()) {
+                entry.getValue().getBasicRemote().sendText("尝试从后端发送消息");
+        }
+    }
+
+    public void SendMessageTest() {
+
+    }
 
     @ResponseBody
     @GetMapping(path = "/getAllProducts")
@@ -33,5 +90,6 @@ public class TestController {
         System.out.println("arrive controller! \n");
         return testDao.getAll();
     }
+
 
 }
