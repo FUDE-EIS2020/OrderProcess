2c7b92f96b35ef21f6dbf2ed506b20a083f9265d
chuyuxuan123 <chuyuxuan@126.com>
2020-06-10 16:48:57 +0800

    feat: implement market and limit order process without test

--- src/main/java/futureTrading/serviceImpl/MarketDepthServiceImpl.java
+++ src/main/java/futureTrading/serviceImpl/MarketDepthServiceImpl.java
@@ -2,20 +2,26 @@ package futureTrading.serviceImpl;
 
 import futureTrading.entities.OrderInMD;
 import futureTrading.service.MarketDepthService;
+import futureTrading.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MarketDepthServiceImpl implements MarketDepthService {
+
+    @Autowired
+    private RedisService redisService;
+
     @Override
     public List<OrderInMD> getBuyOrdersInMD(String brokerId, String productId) {
-        return null;
+        return redisService.splitOrdersInMD(brokerId, productId).get(0);
     }
 
     @Override
     public List<OrderInMD> getSellOrdersInMD(String brokerId, String productId) {
-        return null;
+        return redisService.splitOrdersInMD(brokerId, productId).get(1);
     }
 
     @Override
